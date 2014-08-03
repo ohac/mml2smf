bf93556784eb89317b32bad767e68247be5893f1
OHASHI Hideya <ohachige@gmail.com>
2014-08-03 13:50:18 +0900

    tempo

--- mml2smf.rb
+++ mml2smf.rb
@@ -6,42 +6,58 @@ seq = Sequence.new()
 
 mml = File.open('test.mml', 'r'){|fd|fd.read}
 
-track = nil
-tempo = 120
-reso = 480
+@track = nil
+@tempo = 120
 h = 4
 prog = 1
 velo = 90
 octave = 5
-pending = nil
+@pending = nil
 note = nil
+
+def dopending
+  case @pending
+  when :tempo
+    @track.events << Tempo.new(Tempo.bpm_to_mpq(@tempo))
+  when nil
+  else
+    @track.events << @pending
+  end
+end
 
 mml.split(/\n/).each do |line|
   line.chomp!
   line.split(//).each do |c|
-    unless track
-      track = Track.new(seq)
-      seq.tracks << track
-      track.events << Tempo.new(Tempo.bpm_to_mpq(tempo))
-      track.events << MetaEvent.new(META_SEQ_NAME, 'MML2SMF')
-      track.events << ProgramChange.new(0, prog, 0)
+    unless @track
+      @track = Track.new(seq)
+      seq.tracks << @track
+      @track.events << MetaEvent.new(META_SEQ_NAME, 'MML2SMF')
+      @track.events << ProgramChange.new(0, prog, 0)
     end
     case c
+    when /t/
+      @pending = :tempo
+      @tempo = 0
     when /[cdefgab]/
-      track.events << pending if pending
+      dopending
       note = octave * 12 + 'cdefgab'.index(c)
-      track.events << NoteOn.new(0, note, velo, 0)
+      @track.events << NoteOn.new(0, note, velo, 0)
       len = seq.length_to_delta(4.0 / h)
-      pending = NoteOff.new(0, note, velo, len)
+      @pending = NoteOff.new(0, note, velo, len)
     when /[0-9]/
-      len = seq.length_to_delta(4.0 / c.to_i)
-      pending = NoteOff.new(0, note, velo, len)
-      track.events << pending
-      pending = nil
+      if @pending == :tempo
+        @tempo *= 10
+        @tempo += c.to_i
+      else
+        len = seq.length_to_delta(4.0 / c.to_i)
+        @pending = NoteOff.new(0, note, velo, len)
+        @track.events << @pending
+        @pending = nil
+      end
     when /[ \t\n]/
     end
   end
 end
-track.events << pending if pending
+dopending
 
 File.open('output.mid', 'wb'){|fd| seq.write(fd)}
