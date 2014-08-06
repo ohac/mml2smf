bb69b1032e3514764936074fad2551eaf4dcc94f
OHASHI Hideya <ohachige@gmail.com>
2014-08-06 21:13:07 +0900

    coding ABNF parser

--- abnftest.rb
+++ abnftest.rb
@@ -1,45 +1,51 @@
 #! /usr/bin/env ruby
 require 'abnf'
 mmlfmt = <<EOS
-  mml = 1*(notelen | restlen)
+  mml = *(line | commentline)
+  line = 0*1(*track SP) *(event | WSP) LF
+  commentline = "#" *(WSP | VCHAR) LF
+  track = "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" | "J" | "K" |
+          "L" | "M" | "N" | "O" | "P" | "Q" | "R" | "S" | "T" | "U" | "V" |
+          "W" | "X" | "Y" | "Z"
 EOS
-notelenfmt = <<EOS
+eventfmt = <<EOS
+  event = tiednote | restlen | length | tempo | prog | ch | nexttrack |
+          octave | octavechange | velocity | velocitynext | pan | volume |
+          staccato
+  tiednote = (code | notelen | notenum) 0*(tie (code | notelen | notenum))
+  code = "{" 2*notelen "}"
   notelen = note num | note
-EOS
-notefmt = <<EOS
-  note = 1("a" | "b" | "c" | "d" | "e" | "f" | "g") [("+" | "-" | "=")]
-EOS
-restlenfmt = <<EOS
-  restlen = rest num | rest
-EOS
-restfmt = <<EOS
-  rest = "r"
-EOS
-numfmt = <<EOS
+  notenum = "n" num
+  note = ["~" | "_"] 1("a" | "b" | "c" | "d" | "e" | "f" | "g") [("+" | "-" | "=")]
+  restlen = "r" num | "r"
   num = 1*("0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9")
+  length = "l" num
+  tempo = "t" num
+  tie = "&"
+  prog = "@" num
+  ch = "ch" num
+  nexttrack = ";"
+  octave = "o" num
+  octavechange = "<" | ">"
+  velocity = "v" num
+  velocitynext = "'" num
+  pan = "p" num
+  volume = "v" num
+  staccato = "q" num
 EOS
+
 mml = ABNF.parse(mmlfmt)
-notelen = ABNF.parse(notelenfmt)
-note = ABNF.parse(notefmt)
-restlen = ABNF.parse(restlenfmt)
-rest = ABNF.parse(restfmt)
-num = ABNF.parse(numfmt)
-notelen.merge(num)
-notelen.merge(note)
-restlen.merge(num)
-restlen.merge(rest)
-mml.merge(notelen)
-mml.merge(restlen)
+event = ABNF.parse(eventfmt)
+mml.merge(event)
 mmlr = mml.regexp()
-notelenr = notelen.regexp()
-noter = note.regexp()
-restlenr = restlen.regexp()
-restr = rest.regexp()
-numr = num.regexp()
-str = "c4 d ef r8 g+a-8b=16"
+eventr = event.regexp()
+str = <<EOS
+c4 d ef r8 _g+a-8~b=16 a&a&a n3&n3
+# test
+;
+EOS
 str.gsub(mmlr) do |expr|
-  p [:a, expr]
-  expr.gsub(notelenr) do |n|
-    p [:b, n]
+  expr.gsub(eventr) do |ev|
+    p ev
   end
 end
