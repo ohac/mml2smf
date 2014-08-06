37d2f2f75f286dbc6ed582373c2b0f1233957b69
OHASHI Hideya <ohachige@gmail.com>
2014-08-06 20:31:37 +0900

    coding ABNF parser

--- abnftest.rb
+++ abnftest.rb
@@ -1,25 +1,45 @@
 #! /usr/bin/env ruby
 require 'abnf'
-notefmt = <<EOS
-  expr  = note num | note
+mmlfmt = <<EOS
+  mml = 1*(notelen | restlen)
 EOS
-noteonlyfmt = <<EOS
-  note  = 1("a" | "b" | "c" | "d" | "e" | "f" | "g")
+notelenfmt = <<EOS
+  notelen = note num | note
+EOS
+notefmt = <<EOS
+  note = 1("a" | "b" | "c" | "d" | "e" | "f" | "g") [("+" | "-" | "=")]
+EOS
+restlenfmt = <<EOS
+  restlen = rest num | rest
+EOS
+restfmt = <<EOS
+  rest = "r"
 EOS
 numfmt = <<EOS
-  num   = 1*("0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9")
+  num = 1*("0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9")
 EOS
-r1  = ABNF.parse(notefmt)
-r2  = ABNF.parse(noteonlyfmt)
-r3  = ABNF.parse(numfmt)
-r1.merge(r2)
-r1.merge(r3)
-reg = r1.regexp()
-num = r3.regexp()
-str = "c4 d ef"
-str.gsub(reg) do |expr|
+mml = ABNF.parse(mmlfmt)
+notelen = ABNF.parse(notelenfmt)
+note = ABNF.parse(notefmt)
+restlen = ABNF.parse(restlenfmt)
+rest = ABNF.parse(restfmt)
+num = ABNF.parse(numfmt)
+notelen.merge(num)
+notelen.merge(note)
+restlen.merge(num)
+restlen.merge(rest)
+mml.merge(notelen)
+mml.merge(restlen)
+mmlr = mml.regexp()
+notelenr = notelen.regexp()
+noter = note.regexp()
+restlenr = restlen.regexp()
+restr = rest.regexp()
+numr = num.regexp()
+str = "c4 d ef r8 g+a-8b=16"
+str.gsub(mmlr) do |expr|
   p [:a, expr]
-  expr.gsub(num) do |n|
+  expr.gsub(notelenr) do |n|
     p [:b, n]
   end
 end
